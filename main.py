ef0dfcc74c680dbfd62ff422e667f554be72e5b9
stordd13 <stordeur.bru@gmail.com>
2023-08-07 14:57:44 +0200

    data validation pipeline

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionPipeline
+from mlProject.pipeline.stage_02_data_validation import DataValidationPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -12,3 +13,14 @@ except Exception as e:
     logger.exception(e)
     raise(e)
 
+
+STAGE_NAME = "Data Validation stage"
+
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    obj = DataValidationPipeline()
+    obj.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\n x============x")
+except Exception as e:
+    logger.exception(e)
+    raise(e)
--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,4 +1,4 @@
-from mlProject.entity.config_entity import DataIngestionConfig
+from mlProject.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from mlProject.constants import *
 from mlProject.utils.common import read_yaml, create_directories
 
@@ -28,3 +28,17 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir = config.root_dir,
+            unzip_data_dir = config.unzip_data_dir,
+            STATUS_FILE = config.STATUS_FILE,
+            all_schema = schema
+        )
+        return data_validation_config
